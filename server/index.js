800ecf48ffa4f4664af3d257e0dc59b77b4ee165
Ayush Tiwar <ayush.tiwari0031@gmail.com>
2023-12-28 00:39:14 +0530

    social media app

--- server/index.js
+++ server/index.js
@@ -55,15 +55,13 @@ app.use("/posts", postRoutes);
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URL)//, {useNewUrlParser: true,useUnifiedTopology: true,}
   .then(() => {
+    console.log("mongodb connection successful");
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-
+    
     /* ADD DATA ONE TIME */
-    // User.insertMany(users);
-    // Post.insertMany(posts);
+     //User.insertMany(users);
+    //Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
